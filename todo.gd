f990a8436141bd145b0d1f7aa3487eddf05bd203
Benjamin Sanchez <sky.interphase@gmail.com>
2019-06-27 18:40:32 +0200

    début gui

--- todo.gd
+++ todo.gd
@@ -1,8 +1,7 @@
 """
 # TODO ajouter un dash
 # TODO ajouter du son spatialisé
-# TODO ajouter des récoltables
-# TODO ajouter une UI
+# TODO ajouter une UI pour les pieces et des PV
 # TODO ajouter des destructibles et des ataques
 # TODO ajouter des pièges, des PV, un écran de mort et un menu
 # TODO ajouter un changement de niveau
